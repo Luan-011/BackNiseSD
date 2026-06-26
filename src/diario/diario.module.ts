c4b75c483e541e6b65c1e581b2d793e6c3c1da1a
Luan._073 <joao.216976@fmm.org.br>
2026-06-26 07:33:43 -0400

    fix: corrigindo dependencias circulares nos modulos

--- src/diario/diario.module.ts
+++ src/diario/diario.module.ts
@@ -1,13 +1,16 @@
-// src/diario/diario.module.ts
+import { Injectable, Inject, forwardRef } from '@nestjs/common';import { PrismaService } from '../prisma/prisma.service';
 import { Module } from '@nestjs/common';
 import { DiarioService } from './diario.service';
 import { DiarioController } from './diario.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { IaModule } from 'src/ia/ia.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    forwardRef(() => IaModule) // <--- Importa o IaModule aqui
+  ],
   controllers: [DiarioController],
   providers: [DiarioService],
-  exports: [DiarioService], // <--- ADICIONE ESTA LINHA
+  exports: [DiarioService]
 })
 export class DiarioModule {}
--- src/ia/ia.module.ts
+++ src/ia/ia.module.ts
@@ -1,12 +1,12 @@
-// src/ia/ia.module.ts
+import { Injectable, Inject, forwardRef } from '@nestjs/common';import { PrismaService } from '../prisma/prisma.service';
 import { Module } from '@nestjs/common';
 import { IaService } from './ia.service';
 import { IaController } from './ia.controller';
 import { DiarioModule } from '../diario/diario.module'; // Importe o módulo
 
 @Module({
-  imports: [DiarioModule], // <--- ADICIONE O MÓDULO AQUI
-  controllers: [IaController],
+  imports: [forwardRef(() => DiarioModule)], // Importa o DiarioModule com forwardRef
   providers: [IaService],
+  exports: [IaService] // <--- ESSA LINHA É OBRIGATÓRIA
 })
 export class IaModule {}
